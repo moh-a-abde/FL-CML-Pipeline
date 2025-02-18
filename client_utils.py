9874821484739474f3fb9e1542b270a26a9f8108
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2025-02-18 17:47:19 -0600

    Update client_utils.py

--- client_utils.py
+++ client_utils.py
@@ -142,6 +142,10 @@ class XgbClient(fl.client.Client):
         """
         Perform local model training.
         """
+        y_train = self.train_dmatrix.get_label()
+        class_counts = np.bincount(y_train.astype(int))
+        log(INFO, f"Training data class distribution: Benign={class_counts[0]}, Malicious={class_counts[1]}")
+        
         global_round = int(ins.config["global_round"])
         
         if global_round == 1:
