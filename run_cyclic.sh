01e1cfd08c8bc537739fa921e74d3efd687a70dd
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2025-02-18 18:08:46 -0600

    Update run_cyclic.sh

--- run_cyclic.sh
+++ run_cyclic.sh
@@ -4,11 +4,12 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
 echo "Starting server"
 python3 server.py --train-method=cyclic --pool-size=5 --num-rounds=20 &
-sleep 15  # Sleep for 15s to give the server enough time to start
+sleep 30  # Sleep for 15s to give the server enough time to start
 
 for i in `seq 0 4`; do
     echo "Starting client $i"
     python3 client.py --partition-id=$i --train-method=cyclic --num-partitions=5 --partitioner-type=uniform &
+    sleep 5
 done
 
 # Enable CTRL+C to stop all background processes
