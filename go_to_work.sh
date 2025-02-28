cc248a09905ae9113ca0d10a68b2a9057d1e8a40
abde8473 <abde8473@stthomas.edu>
2025-02-27 23:48:32 -0500

    commit.sh

--- go_to_work.sh
+++ go_to_work.sh
@@ -16,16 +16,4 @@ kill $PID1 $PID2
 # Run Git commands and GitHub workflow
 git pull
 ./commit.sh
-gh workflow run cml.yaml
-
-# Wait for 5 minutes
-sleep 300
-
-# Run Git commands again
-git pull
-./commit.sh
-
-# Open the evaluation results folder
-#xdg-open /path/to/eval_results  # For Linux
-# open /path/to/eval_results    # Uncomment this if you're on macOS
 
