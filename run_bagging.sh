03c7a39f5f5fd0335b1315244490f29239fc63ff
moh-a-abde <abde8473@stthomas.edu>
2025-04-22 16:13:37 -0500

    commit.sh

--- run_bagging.sh
+++ run_bagging.sh
@@ -12,7 +12,20 @@ python3 client.py --partition-id=0 --num-partitions=2 --partitioner-type=exponen
 
 # Start regular client (partition 1)
 echo "Starting regular client (partition 1)"
-python3 client.py --partition-id=1 --num-partitions=2 --partitioner-type=exponential &
+python3 client.py --partition-id=1 --num-partitions=5 --partitioner-type=exponential &
+
+# Start regular client (partition 2)
+echo "Starting regular client (partition 2)"
+python3 client.py --partition-id=2 --num-partitions=5 --partitioner-type=exponential &
+
+# Start regular client (partition 3)
+echo "Starting regular client (partition 3)"
+python3 client.py --partition-id=3 --num-partitions=5 --partitioner-type=exponential &
+
+# Start regular client (partition 4)
+echo "Starting regular client (partition 4)"
+python3 client.py --partition-id=4 --num-partitions=5 --partitioner-type=exponential &
+
 
 # Enable CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
