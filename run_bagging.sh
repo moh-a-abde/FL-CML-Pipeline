2cdf7e0c944c8dd497b728e197a64aa73300c4ff
moh-a-abde <abde8473@stthomas.edu>
2025-05-07 10:04:39 -0500

    commit.sh

--- run_bagging.sh
+++ run_bagging.sh
@@ -3,7 +3,7 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
 echo "Starting server"
-python3 server.py --pool-size=5 --num-rounds=5 --num-clients-per-round=5 &
+python3 server.py --pool-size=5 --num-rounds=5 --num-clients-per-round=5 --centralised-eval &
 sleep 30  # Sleep for 30s to give the server enough time to start
 
 # Start regular client (partition 0)
